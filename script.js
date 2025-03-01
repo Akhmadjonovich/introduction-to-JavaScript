2f42fbe325b7adb11e3b5f6e89ab4b1b192bf437
Akhmadjonovich <axrorbeknematov07@gmail.com>
2025-03-01 09:19:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let a = prompt("Ismingizni kiritin")
+
+let b= "  yosh "
+
+let d= "  "
+
+const c = prompt(" Yoshingizni kiriting")
+
+alert(a+d+c+b)
+
+console.log(a+d+c+b)
+
+let num1 = +prompt("1-Sonni kiriting")
+let num2 = +prompt("2-Sonni kiriting")
+alert(num1+num2)
+
+let yil = prompt("Tug'ilgan yilingizni kiriting");
+
+yil = Number(yil);
+alert(2025-yil)
+
+let son = prompt(" biror son kiriting (kvadrat va kubga oshirish uchun");
+
+son = Number(son);
+
+
+let kvadrat = son ** 2;
+let kub = son ** 3;
+
+
+
+console.log(`Kvadrati: ${kvadrat}`);
+console.log(`Kubi: ${kub}`);
+
